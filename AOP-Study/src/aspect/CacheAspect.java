f075e32d4c97661519e07d0bdb047cb37dac89c3
StartDeveloperKim <ktw550111@naver.com>
2022-07-25 19:53:37 +0900

    @Order() 어노테이션을 이용하여 프록시의 순서를 지정할 수 있다.

--- AOP-Study/src/aspect/CacheAspect.java
+++ AOP-Study/src/aspect/CacheAspect.java
@@ -7,27 +7,30 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
 
 @Aspect
+@Order(2)
 public class CacheAspect {
-	
+
 	private Map<Long, Object> cache = new HashMap<>();
-	
+
 	@Pointcut("execution(public * chapter7..*(long))")
 	public void cacheTarget() {
 	}
 	
-	@Around("cacheTarget")
-	public Object execute(ProceedingJoinPoint jointPoint) throws Throwable {
-		Long num = (Long) jointPoint.getArgs()[0];
-		if(cache.containsKey(num)) {
+	@Around("cacheTarget()")
+	public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+		Long num = (Long) joinPoint.getArgs()[0];
+		if (cache.containsKey(num)) {
 			System.out.printf("CacheAspect: Cache에서 구함[%d]\n", num);
 			return cache.get(num);
 		}
-		
-		Object result = jointPoint.proceed();
+
+		Object result = joinPoint.proceed();
 		cache.put(num, result);
 		System.out.printf("CacheAspect: Cache에 추가[%d]\n", num);
 		return result;
 	}
+
 }
--- AOP-Study/src/aspect/ExeTimeAspect.java
+++ AOP-Study/src/aspect/ExeTimeAspect.java
@@ -7,8 +7,10 @@ import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
 
 @Aspect
+@Order(1)
 public class ExeTimeAspect {
 	
 	@Pointcut("execution(public * chapter7..*(..))") // 공통 기능을 적용할 대상을 설정한다.
