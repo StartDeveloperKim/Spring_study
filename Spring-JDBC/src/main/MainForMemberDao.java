2bf93e21c200a6c20c2b67bae96a27990457f059
StartDeveloperKim <ktw550111@naver.com>
2022-07-26 18:33:47 +0900

    패스워드를 바꾸기 위한 트랜잭션 클래스를 생성하였고 이에대한 main함수 변경

--- Spring-JDBC/src/main/MainForMemberDao.java
+++ Spring-JDBC/src/main/MainForMemberDao.java
@@ -8,8 +8,10 @@ import java.util.List;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import config.AppCtx;
+import spring.ChangePasswordService;
 import spring.Member;
 import spring.MemberDao;
+import spring.MemberNotFoundException;
 import spring.WrongIdPasswordException;
 
 public class MainForMemberDao {
@@ -21,9 +23,22 @@ public class MainForMemberDao {
 		
 		memberDao = ctx.getBean(MemberDao.class);
 		
+		ChangePasswordService cps = 
+				ctx.getBean("changePwdSvc", ChangePasswordService.class);
+		
+		try {
+			cps.changePassword("ktw@naver.com", "0000", "1234");
+			System.out.println("암호를 변경했습니다.");
+		} catch (MemberNotFoundException e) {
+            System.out.println("회원 데이터가 존재하지 않습니다.");
+		} catch (WrongIdPasswordException e) {
+			 System.out.println("암호가 올바르지 않습니다.");
+		}
+		
 		selectAll();
-		updateMember();
-		insertMember();
+		/*
+		 * selectAll(); updateMember(); insertMember();
+		 */
 		
 		ctx.close();
 
---
+++ chapter2/src/chapter2/spring/ChangePasswordService.java
@@ -0,0 +1,26 @@
+package chapter2.spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ChangePasswordService {
+	
+	@Autowired
+	private MemberDao memberDao;
+	
+	public void changePassword(String email, String oldPwd, String newPwd) throws MemberNotFoundException, WrongIdPasswordException {
+		Member member = memberDao.selectByEmail(email);
+		if(member == null)
+			throw new MemberNotFoundException();
+		
+		member.changePassword(oldPwd, newPwd);
+		
+		memberDao.update(member);
+	}
+	
+	public void setMemberDao(MemberDao memberDao) {
+		this.memberDao = memberDao;
+	}
+	
+}
