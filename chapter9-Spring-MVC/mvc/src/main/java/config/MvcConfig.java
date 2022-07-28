93115a30170df449fc020e73b61a5c95d77e0719
StartDeveloperKim <ktw550111@naver.com>
2022-07-28 17:13:38 +0900

    addViewControllers는 컨트롤러 클래스를 만들지 않더라고 해당 url로 접속하면 해당되는 .jsp 파일을 보여준다

--- chapter9-Spring-MVC/mvc/src/main/java/config/MvcConfig.java
+++ chapter9-Spring-MVC/mvc/src/main/java/config/MvcConfig.java
@@ -3,6 +3,7 @@ package config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -19,4 +20,10 @@ public class MvcConfig implements WebMvcConfigurer{
 	public void configureViewResolvers(ViewResolverRegistry registry) {
 		registry.jsp("/WEB-INF/view/", ".jsp");
 	} // jsp 경로 
+	
+	
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/main").setViewName("main");
+	}
 }
