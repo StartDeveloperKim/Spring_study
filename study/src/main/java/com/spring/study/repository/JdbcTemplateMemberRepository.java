e70cc9663412b0ced2c2f4e7b70836e833094721
StartDeveloperKim <ktw550111@naver.com>
2022-08-15 23:22:52 +0900

    [업데이트] - 아이디 중복체크를 위해 DAO부분에 id/nickname을 통해 Member를 구해오는 쿼리를 작성

--- study/src/main/java/com/spring/study/repository/JdbcTemplateMemberRepository.java
+++ study/src/main/java/com/spring/study/repository/JdbcTemplateMemberRepository.java
@@ -1,13 +1,17 @@
 package com.spring.study.repository;
 
+import com.spring.study.domain.MemberDTO;
 import com.spring.study.domain.RegisterDTO;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.RowMapper;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 public class JdbcTemplateMemberRepository implements MemberRepository {
 
@@ -18,6 +22,8 @@ public class JdbcTemplateMemberRepository implements MemberRepository {
     }
 
     private final String insert_sql = "insert into MEMBER2 (ID, PASSWORD, NAME, NICKNAME) values (?, ?, ?, ?)";
+    private final String getMemberById_sql = "select * from MEMBER2 where id = ?";
+    private final String getMemberByNickname_sql = "select * from MEMBER2 where nickname = ?";
 
     @Override
     public void insert(RegisterDTO registerDTO) {
@@ -35,4 +41,28 @@ public class JdbcTemplateMemberRepository implements MemberRepository {
             }
         });
     }
+
+    @Override
+    public boolean checkIdDuplicate(String id) {
+        List<MemberDTO> results = jdbcTemplate.query(getMemberById_sql, rowMapper, id);
+        if (!results.isEmpty()){
+            return false;
+        }
+        return true;
+    }
+
+    private RowMapper<MemberDTO> rowMapper = new RowMapper<MemberDTO>() {
+        @Override
+        public MemberDTO mapRow(ResultSet rs, int rowNum) throws SQLException {
+            MemberDTO memberDTO = new MemberDTO(
+                    rs.getString("ID"),
+                    rs.getString("PASSWORD"),
+                    rs.getString("WRITER"),
+                    rs.getString("NICKNAME"),
+                    rs.getDate("REGDATE")
+            );
+
+            return memberDTO;
+        }
+    };
 }
--- study/src/main/java/com/spring/study/repository/MemberRepository.java
+++ study/src/main/java/com/spring/study/repository/MemberRepository.java
@@ -5,4 +5,6 @@ import com.spring.study.domain.RegisterDTO;
 public interface MemberRepository {
 
     public void insert(RegisterDTO registerDTO); // 멤버 등록
+
+    public boolean checkIdDuplicate(String id); // 아이디 중복체크
 }
