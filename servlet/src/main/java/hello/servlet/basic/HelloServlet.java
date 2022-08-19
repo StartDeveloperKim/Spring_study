e83f3f209aa4e92e1b9347d56976dec396bd3efd
StartDeveloperKim <ktw550111@naver.com>
2022-08-19 20:26:25 +0900

    [공부] - Servlet 요청/응답에 대한 파일

---
+++ servlet/src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,27 @@
+package hello.servlet.basic;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain"); // ContentType 설정
+        response.setCharacterEncoding("utf-8"); // Encoding 설정
+        response.getWriter().write("하이 " + username); // response 객체에 데이터 넣기
+    }
+}
