2fcc9d41c358505da13e6c8238cc860142cea4eb
StartDeveloperKim <ktw550111@naver.com>
2022-08-22 20:00:00 +0900

    [공부] - Http 메시지 바디 내용 조회하기

---
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
@@ -0,0 +1,61 @@
+package hello.springmvc.basic.request;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+
+@Slf4j
+@Controller
+public class RequestBodyStringController {
+
+    @PostMapping("/request-body-string-v1")
+    public void requestBodyStringV1(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        ServletInputStream inputStream = request.getInputStream();
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        log.info("messageBody = {}", messageBody);
+
+        response.getWriter().write("ok");
+    }
+
+    @PostMapping("/request-body-string-v2")
+    public void requestBodyStringV2(InputStream inputStream, Writer writer) throws IOException {
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        log.info("messageBody = {}", messageBody);
+
+        writer.write("ok");
+    }
+
+    @PostMapping("/request-body-string-v3")
+    public HttpEntity<String> requestBodyStringV3(HttpEntity<String> httpEntity) throws IOException {
+        /*HttpEntity를 사용하면 위의 복잡한 과정없이 messageBody를 받을 수 있고
+        * 반환도 HttpEntity로 하여 JSON 또는 Text를 보낼 수 있다.*/
+        String messageBody = httpEntity.getBody();
+        log.info("messageBody = {}", messageBody);
+
+        return new HttpEntity<>("ok");
+    }
+
+    @ResponseBody
+    @PostMapping("/request-body-string-v4")
+    public String requestBodyStringV4(@RequestBody String messageBody) {
+        /*RequestBody만 있으면 Http의 messageBody를 바로 읽어버린다.*/
+        log.info("messageBody = {}", messageBody);
+
+        return "ok";
+    }
+
+}
