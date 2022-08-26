ef6cf0dde207ea4169f276d9c7dae85c81e3863f
StartDeveloperKim <ktw550111@naver.com>
2022-08-26 13:02:16 +0900

    타임리프 공부

--- study/src/test/java/com/spring/study/service/MemberRegisterServiceImplTest.java
+++ study/src/test/java/com/spring/study/service/MemberRegisterServiceImplTest.java
@@ -18,12 +18,13 @@ class MemberRegisterServiceImplTest {
     @Autowired
     MemberRepository memberRepository;
 
+
     @Test
     @DisplayName("회원 등록 서비스 테스트입니다.")
     void MemberRegisterServiceTest(){
         // 중복 아이디 체크 기능을 만들자 --> 버튼 형식
         RegisterDTO registerDTO = new RegisterDTO();
-        registerDTO.setId("kims00");
+        registerDTO.setId("ms00");
         registerDTO.setPassword("0000");
         registerDTO.setName("김태우");
         registerDTO.setNickname("킹태우");
