33c2ec929d1857c3a897b7e9324fd7d0b1a4eedd
StartDeveloperKim <ktw550111@naver.com>
2022-09-05 19:13:53 +0900

    오류페이지 공부 ==> 서블릿 필터에서는 dispatcherType을 이용해서 필터를 중복으로 실행되지 않게 할 수 있고 인터셉터에서는 excludeUriPatterns를 이용하여 필터를 중복실행을 피할 수 있다. 그런데 스프링부트에서는 기본적으로 오류페이지에 대한 컨트롤러를 내장하고 있어서 정해진경로에 오류페이지에 대한 view만 만들어주면 view가 자동으로 반환된다. 와우,,

--- item-service/src/main/java/hello/itemservice/web/basic/HomeController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/HomeController.java
@@ -3,6 +3,7 @@ package hello.itemservice.web.basic;
 import hello.itemservice.domain.member.Member;
 import hello.itemservice.domain.member.MemberRepository;
 import hello.itemservice.web.SessionConst;
+import hello.itemservice.web.argumentresolver.Login;
 import hello.itemservice.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -49,9 +50,22 @@ public class HomeController {
      * <p>@SessionAttribute 어노테이션에서 원래의 로직이 모두 수행된다.</p>
      * 일반사용자도 home에는 올 수 있기때문에 required는 false로 설정한다.
      */
-    @GetMapping("/")
+    /*@GetMapping("/")
     public String homeLogin(
             @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
+
+        // 세션에 회원 데이터가 없으면 home
+        if (loginMember == null) {
+            return "home";
+        }
+
+        //세션이 유지되면 로그인으로 이동
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }*/
+
+    @GetMapping("/")
+    public String homeLoginArgumentResolver(@Login Member loginMember, Model model) {
 
         // 세션에 회원 데이터가 없으면 home
         if (loginMember == null) {
