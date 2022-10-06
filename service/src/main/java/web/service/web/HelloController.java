dba4b325090e647e34c351d22965877eee89fddb
StartDeveloperKim <ktw550111@naver.com>
2022-10-06 19:41:13 +0900

    스프링 부트와 AWS로 혼자 구현하는 웹 서비스 책을 시작하려한다. 기본적인 내부내용에서 게시판 만들기 등은 해보았는데 TDD(테스트 주도 개발) 또는 AWS 배포 등의 내용은 한번도 다뤄보지 못했기 때문에 기대되는 부분이다. 10월 둘째주 안으로 이 책을 마무리 지어야 겠다.

---
+++ service/src/main/java/web/service/web/HelloController.java
@@ -0,0 +1,17 @@
+package web.service.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto hello(@RequestParam("name") String name,
+                        @RequestParam("amount") int amount) {
+
+        return new HelloResponseDto(name, amount);
+    }
+
+}
---
+++ service/src/test/java/web/service/web/HelloControllerTest.java
@@ -0,0 +1,43 @@
+package web.service.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(controllers = HelloController.class)
+class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void hello_가리턴된다() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+
+    @Test
+    void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(
+                get("/hello/dto")
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect((ResultMatcher) jsonPath("$.name", is(name)))
+                .andExpect((ResultMatcher) jsonPath("$.amount", is(amount))
+
+                );
+    }
+}
